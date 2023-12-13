6b40d761502428d0987c1e9e6f048d2b540b968b
Niladree Datta <62564128+Niladree60@users.noreply.github.com>
2023-12-13 23:47:20 +0600

    Create LeetCode52.cpp

---
+++ LeetCode52.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    set<int> cols, negDiag, posDiag;
+    int totalNQueens(int n) {
+        vector<vector<string>> res;
+        vector<string> board(n,string(n, '.'));
+        backtrack(n,0,res,board);
+        return res.size();
+    }
+    void backtrack(int n, int row, vector<vector<string>> &res, vector<string>&board)
+    {
+        if(row == n)
+        {
+            res.push_back(board);
+            return;
+        }
+        for(int col = 0 ; col < n ; col++)
+        {
+            if(cols.count(col) == 1 or negDiag.count(row-col) or posDiag.count(row+col))
+            {
+                continue;
+            }
+            cols.insert(col);
+            negDiag.insert(row-col);
+            posDiag.insert(row+col);
+            board[row][col] = 'Q';
+
+            backtrack(n,row+1,res,board);
+
+            cols.erase(col);
+            negDiag.erase(row-col);
+            posDiag.erase(row+col);
+            board[row][col] = '.';
+        }
+    }
+};
