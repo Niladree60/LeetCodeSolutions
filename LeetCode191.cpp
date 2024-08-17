ee207fc98a03e29eb3c764978b35e5ff6847db13
Niladree Datta <62564128+Niladree60@users.noreply.github.com>
2024-08-17 22:44:36 +0600

    Create LeetCode191.cpp

---
+++ LeetCode191.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        
+        int cnt = 0;
+        while(n)
+        {
+            cnt += (1&n);
+            n = n/2;
+        }
+        return cnt;
+    }
+};
