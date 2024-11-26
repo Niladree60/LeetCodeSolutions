c0f316bbcb980490b58375ee402bf07906dfb14a
Niladree Datta <62564128+Niladree60@users.noreply.github.com>
2024-11-26 09:23:28 +0600

    Create LeetCode257.cpp

---
+++ LeetCode257.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> vc;
+
+    void dfs(TreeNode* root, string s) {
+        if(!root -> left and !root -> right) {
+            vc.push_back(s);
+            return;
+        }
+
+        if(root -> left) {
+            dfs(root -> left, s + "->" + to_string(root -> left -> val));
+        }
+        if(root -> right) {
+            dfs(root -> right, s + "->" + to_string(root -> right -> val));
+        }
+    }
+
+    vector<string> binaryTreePaths(TreeNode* root) {
+        if(!root) return vc;
+
+        dfs(root, to_string(root -> val));
+        return vc;
+    }
+};
