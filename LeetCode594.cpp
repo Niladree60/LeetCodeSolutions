3681dec2422bb39f856d4fb78300ffedcc99df27
Niladree Datta <62564128+Niladree60@users.noreply.github.com>
2025-01-25 01:48:20 +0600

    Create LeetCode594.cpp

---
+++ LeetCode594.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        map<int,int>m;
+        int res = 0;
+
+        for(auto i: nums){
+            m[i]++;
+
+            if(m.count(i+1))
+                res = max(res, m[i] + m[i+1]);
+
+            if(m.count(i-1))
+                res = max(res, m[i] + m[i-1]);
+        }
+        
+        return res;        
+    }
+};
